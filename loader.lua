25d56b6d096f546d65a7f4d1cbabacd8a49b58f4
synolope <98623199+synolope@users.noreply.github.com>
2022-05-09 21:16:14 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -6,5 +6,7 @@ local function ontp(s)
     end
 end
 
+game.ReplicatedStorage:WaitForChild("Constants")
+
 loadstring(game:HttpGet('https://raw.githubusercontent.com/synolope/meepcracked/main/script.lua'))()
 ontp("if not game:IsLoaded() then game.Loaded:Wait() end wait(3) if not game.Players.LocalPlayer.Character then game.Players.LocalPlayer.CharacterAdded:Wait() end loadstring(game:HttpGet('https://raw.githubusercontent.com/synolope/meepcracked/main/loader.lua'))()")
